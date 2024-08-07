e959954685195249ae95b237f886a9d842218f45
sowjanya-1105 <you@example.com>
2024-08-07 23:23:36 +0530

    added new changes

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,69 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // e function lekapote by default call aypotadi painadi
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("bydefault call");
+
     const check = document.getElementById("check");
-    const pass = document.getElementById("pass");
+    const user_password = document.getElementById("user_password");
 
     check.addEventListener("click", togglepassword);
 
     function togglepassword() {
         if (check.checked) {
-            
-            pass.type = "text";
+
+            user_password.type = "text";
         } else {
-            pass.type = "password";
+            user_password.type = "password";
         }
     }
 
-
 });
+
+
+
+let email = "test@gmail.com";
+let password = 123456;
+
+
+console.log("kkkk", document.getElementById("submit_login_form"));
+
+document.getElementById("user_email_error").textContent = "";
+
+
+document.getElementById("submit_login_form").addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+
+    let userEnteredEmail = document.getElementById("user_email").value;
+    let userEnteredPassword = document.getElementById("user_password").value;
+
+
+    if (userEnteredEmail === "") {
+        document.getElementById("user_email_error").textContent = "Enter email";
+        return
+        
+    }
+
+
+    if (userEnteredPassword === "") {
+        document.getElementById("user_password_error").textContent = "Enter password";
+        return
+    }
+
+
+    console.log("details", userEnteredEmail, userEnteredPassword);
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
