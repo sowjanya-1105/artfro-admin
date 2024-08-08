3ddd6d25baee793cd91c60f3d6ef74ef3ac76059
sowjanya-1105 <you@example.com>
2024-08-08 21:38:46 +0530

    home page

--- js/custom.js
+++ js/custom.js
@@ -15,49 +15,103 @@ document.addEventListener("DOMContentLoaded", function () {
             user_password.type = "password";
         }
     }
-
 });
 
 
 
-let email = "test@gmail.com";
-let password = 123456;
 
 
-console.log("kkkk", document.getElementById("submit_login_form"));
+    let userEnteredEmail = document.getElementById("user_email");
+    let userEnteredPassword = document.getElementById("user_password");
 
-document.getElementById("user_email_error").textContent = "";
+    userEnteredEmail.addEventListener('input', (evt) => {
 
+        if (evt.target.value === "") {
+            document.getElementById("user_email_error").textContent = "Enter email"
+        }
 
-document.getElementById("submit_login_form").addEventListener("submit", function (event) {
+        else {
+            document.getElementById("user_email_error").textContent = ""
+        }
 
-    event.preventDefault();
-
-
-    let userEnteredEmail = document.getElementById("user_email").value;
-    let userEnteredPassword = document.getElementById("user_password").value;
-
-
-    if (userEnteredEmail === "") {
-        document.getElementById("user_email_error").textContent = "Enter email";
-        return
-        
-    }
+    })
 
 
-    if (userEnteredPassword === "") {
-        document.getElementById("user_password_error").textContent = "Enter password";
-        return
-    }
 
+    userEnteredPassword.addEventListener('input', (evt) => {
 
-    console.log("details", userEnteredEmail, userEnteredPassword);
+        if (evt.target.value === "") {
+            document.getElementById("user_password_error").textContent = "Enter password"
+        }
+        else {
+            document.getElementById("user_password_error").textContent = ""
+        }
+
+    })
 
 
 
 
 
-})
+
+
+    let email = "test@gmail.com";
+    let password = 123456;
+
+
+    // console.log("kkkk", document.getElementById("submit_login_form"));
+
+
+
+
+    document.getElementById("submit_login_form").addEventListener("submit", function (event) {
+
+        event.preventDefault();
+
+        let email = 'test@gmail.com';
+        let password = 123456;
+        console.log("email", email, password);
+
+
+        if (userEnteredEmail.value === "") {
+            document.getElementById("user_email_error").textContent = "Enter email";
+            return
+
+
+        } else if (userEnteredEmail.value != email) {
+            document.getElementById("user_email_error").textContent = "invalid email"
+            return
+
+        }
+       
+
+        if (userEnteredPassword.value === "") {
+            document.getElementById("user_password_error").textContent = "Enter password";
+            return;
+        } else if (userEnteredPassword.value != password) {
+            document.getElementById("user_password_error").textContent = "invalid password"
+        } else if(userEnteredEmail.value == email) {
+            userEnteredEmail.value = "";
+            userEnteredPassword.value = "";
+            // alert("submitted successfully")
+
+            window.location.href = "home.html";
+
+        }
+
+      
+
+    });
+
+   
+
+
+
+
+
+
+
+
 
 
 
